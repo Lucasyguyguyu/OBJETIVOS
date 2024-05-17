9ea96f1521fb2b997e1c56c3fd180043f5630d6c
School Dev <112954598+Lucasyguyguyu@users.noreply.github.com>
2024-05-17 11:37:30 -0300

    Add files via upload

---
+++ Lucas 3B/Lucas 3b/code.js
@@ -0,0 +1,13 @@
+const botoes = document.querySelectorAll(".obj");
+
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function () {
+                {
+            botoes[j].classList.add(".ativo");
+        }
+    }
+}
+    
+    
+
